1af116a9cfd0290e4b27d55df31370d0fc73daf1
Дамир Хамитов <hamitov.damir3101@icloud.com>
2022-05-20 23:34:14 +0600

    Added ascii aperture science logo

--- Source/Kernel/kernel.c
+++ Source/Kernel/kernel.c
@@ -7,6 +7,6 @@ void kernel_main()
     ClearScreen(BACKGROUND_BLACK | FOREGROUND_WHITE);
     EnableCursor(13, 15);
     SetCursorPosition(PosFromCoords(0, 0));
-    PrintString(file);
+    PrintStringColored(file, BACKGROUND_BLACK | FOREGROUND_YELLOW);
     return;
 }
